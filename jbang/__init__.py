39b0a6209b064fbf04d196195810b04fd80f963e
Max Rydahl Andersen <max.andersen@gmail.com>
2023-09-11 11:26:03 +0200

    update to autopublish on release + logging fixes

--- jbang/__init__.py
+++ jbang/__init__.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
 import platform
+import logging
+
+log = logging.getLogger(__name__)
 
 def exec(*args):
     arg_line = " ".join(args)
@@ -15,18 +18,18 @@ def exec(*args):
     powershell_available = subprocess.run(["which", "powershell"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode == 0
     
     if jbang_available:
-        print(f"using jbang: {arg_line}")
+        log.debug(f"using jbang: {arg_line}")
         cmd_result = subprocess.run(f"jbang {arg_line}", shell=True, capture_output=True, text=True)
     elif curl_available and bash_available:
-        print(f"using curl + bash: {arg_line}")
+        log.debug(f"using curl + bash: {arg_line}")
         cmd_result = subprocess.run(f"curl -Ls https://sh.jbang.dev | bash -s - {arg_line}", shell=True, capture_output=True, text=True)
     elif powershell_available:
-        print(f"using powershell: {arg_line}")
+        log.debug(f"using powershell: {arg_line}")
         subprocess.run('echo iex "& { $(iwr -useb https://ps.jbang.dev) } $args" > %TEMP%/jbang.ps1', shell=True)
         cmd_result = subprocess.run(f'powershell -Command "%TEMP%/jbang.ps1 {arg_line}"', shell=True, capture_output=True, text=True)
     else:
-        print(f"unable to pre-install jbang: {arg_line}")
-        exit(1)
+        log.debug(f"unable to pre-install jbang: {arg_line}")
+        raise Exception(f"Unable to pre-install jbang using '{arg_line}'. Please install jbang manually and try again. See https://jbang.dev for more information.")
     
     if cmd_result.returncode != 0:
         raise Exception(f"The command failed: 'jbang {arg_line}'. Code: {cmd_result.returncode}, Stderr: {cmd_result.stderr}")
